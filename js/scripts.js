24e05b3c70d25679c386df330c133f1adf0f2933
JoudHasan <129724393+JoudHasan@users.noreply.github.com>
2023-05-23 12:05:07 +0200

    added if

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,14 @@ let pokemonList = [
   }
 ];
 
+const heightThreshold = 6; // Set the height threshold for special Pokémon
+
 for (let i = 0; i < pokemonList.length; i++) {
   let pokemon = pokemonList[i];
   let pokemonInfo = pokemon.name + " (height: " + pokemon.height + ")";
+if (pokemon.height > heightThreshold) {
+    pokemonInfo += " - Wow, that's big!";
+  }
   document.write(pokemonInfo + "<br>");
 }
 
