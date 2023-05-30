acfce17eecd9ed2d80ddfbae6d7765622978eb32
JoudHasan <129724393+JoudHasan@users.noreply.github.com>
2023-05-30 08:33:11 +0200

    iife and forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,50 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.6,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Charizard",
-    height: 1.9,
-    types: ["fire"]
-  },
-  {
-    name: "Squirtle",
-    height: 1.5,
-    types: ["water"]
-  }
-];
+const pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.6,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Charizard",
+      height: 1.9,
+      types: ["fire"]
+    },
+    {
+      name: "Squirtle",
+      height: 1.5,
+      types: ["water"]
+    }
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >= 1.6) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
-  } else if (pokemonList[i].height < 1.6 && pokemonList[i].height >= 1.0) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - This is an average size.");
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height >= 1.6) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!");
+  } else if (pokemon.height < 1.6 && pokemon.height >= 1.0) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - This is an average size.");
   } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - This is very small!");
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - This is very small!");
   }
   document.write("<br>");
-}
-}
+});
+
+// Example usage of add() function
+pokemonRepository.add({
+  name: "Squirtle",
+  height: 1.5,
+  types: ["water"]
+});
